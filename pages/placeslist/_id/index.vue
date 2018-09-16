19958b06bb1769782ed857d99a73f18e0be57ac0
Екатерина Лебедева <katty@Ekaterinas-MacBook-Pro.local>
2018-09-17 01:45:55 +0300

    create project tree

---
+++ pages/placeslist/_id/index.vue
@@ -0,0 +1,6 @@
+<template>
+	<h1>A single Place with ID: {{ $route.params.id }}</h1>
+</template>
+
+<script>
+</script>
---
+++ pages/placesmap/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Header />
+
+        <h1>Places_map</h1>
+    </div>
+</template>
+
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
